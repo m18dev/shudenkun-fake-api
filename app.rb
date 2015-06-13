de6611146f7dd0e62dda2474aa48d45f193f63b9
maeharin <maeharin@gmail.com>
2015-06-13 17:03:45 +0900

    add api

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
   'Hello world!'
+end
+
+get '/api/last_train.json' do
+  content_type :json
+
+  { 
+    depature: "溜池山王",
+    destination: "五反野",
+    depature_at: "23:56",
+    remain_min: 438
+  }.to_json
 end
